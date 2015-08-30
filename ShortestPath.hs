101c5905aace1c351214f5dfc6719edc0c99c84e
ToF- <cthibaut@octo.com>
2015-08-30 19:56:47 +0200

    changed types to data

--- ShortestPath.hs
+++ ShortestPath.hs
@@ -2,30 +2,30 @@ module ShortestPath
 where
 import Data.PSQueue
 
-type WeighedGraph k w = [(k,[(k,w)])]
-type Path k w = [(k,w)]
+data Graph k w = Graph [(k,[(k,w)])]
+data Path k w = Path [(k,w)] deriving (Eq,Show)
 
 type Distances k w = PSQ k w
 
-shortestPath :: Ord (k) => Ord (w) => Num w => WeighedGraph k w -> k -> k -> Path k w
-shortestPath g f t | f == t    = [(f,0)]
+shortestPath :: (Num w,Ord k) => Graph k w -> k -> k -> Path k w
+shortestPath (Graph g) f t | f == t    = Path [(f,0)]
                    | otherwise = case Prelude.lookup f g >>= Prelude.lookup t of
-                        Just n -> [(t,n)]
+                        Just n -> Path [(t,n)]
 
-initialDistances :: Ord k => Ord w => Num w => WeighedGraph k w -> k -> PSQ k w
-initialDistances g k = adjust (const 0) k (fromList (map (\(n,_) -> n :-> 10000000) g))
+initialDistances :: (Ord k, Num w,Ord w) => Graph k w -> k -> PSQ k w
+initialDistances (Graph g) k = adjust (const 0) k (fromList (map (\(n,_) -> n :-> 10000000) g))
 
-updateDistance :: Ord k => Ord w => Num w => PSQ k w  -> k -> w -> PSQ k w
+updateDistance :: (Ord k, Ord w) => PSQ k w  -> k -> w -> PSQ k w
 updateDistance ds k w = adjust (min w) k ds
 
-shortestPathStep :: Ord k => Ord w => Num w => WeighedGraph k w -> (PSQ k w, Path k w) -> (PSQ k w, Path k w) 
-shortestPathStep g (ds,p) = case minView ds of
-    Nothing -> (ds,p)
+shortestPathStep :: (Ord w, Num w, Ord k) => Graph k w -> (PSQ k w, Path k w) -> (PSQ k w, Path k w) 
+shortestPathStep (Graph g) (ds,Path p) = case minView ds of
+    Nothing -> (ds,Path p)
     Just (k :-> w, ds') -> let adj = Prelude.lookup k g
                                update ds (n,d) = updateDistance ds n (w+d)
                                ds''= case adj of
                                    Just ks -> Prelude.foldl update ds' ks
                                    Nothing -> ds'
-                           in (ds'', p++[(k,w)]) 
+                           in (ds'', Path (p++[(k,w)])) 
 
                             
--- Specs.hs
+++ Specs.hs
@@ -3,18 +3,21 @@ import Sample
 import ShortestPath
 import Data.PSQueue
 
+rs = Graph roads
+
+
 main = hspec $ do
     describe "shortest path" $ do
         it "from a node to itself is 0" $ do
-            shortestPath roads Blaxhall Blaxhall `shouldBe` [(Blaxhall,0)] 
-            shortestPath roads Clacton Clacton `shouldBe` [(Clacton,0)] 
+            shortestPath rs Blaxhall Blaxhall `shouldBe` Path [(Blaxhall,0)] 
+            shortestPath rs Clacton Clacton `shouldBe` Path [(Clacton,0)] 
 
         it "from a node to a connected node is the weigh" $ do
-            shortestPath roads Blaxhall Dunwich `shouldBe` [(Dunwich,15)]
+            shortestPath rs Blaxhall Dunwich `shouldBe` Path [(Dunwich,15)]
 
     describe "initial distances" $ do
         it "from a node yields all infinity except node" $ do
-            toList (initialDistances roads Blaxhall) `shouldBe`
+            toList (initialDistances rs Blaxhall) `shouldBe`
                 [Blaxhall :-> 0
                 ,Clacton :-> 10000000
                 ,Dunwich :-> 10000000
@@ -25,7 +28,7 @@ main = hspec $ do
 
     describe "update distance" $ do
         it "update the distance table for a node if lower" $ do
-            let ds = initialDistances roads Blaxhall 
+            let ds = initialDistances rs Blaxhall 
             toList (updateDistance ds Dunwich 15)  `shouldBe` 
                 [Blaxhall :-> 0
                 ,Clacton :-> 10000000
@@ -35,7 +38,7 @@ main = hspec $ do
                 ,Maldon  :-> 10000000
                 ,Tiptree :-> 10000000]
         it "doesn't update the distance table for a node if higher" $ do
-            let ds = initialDistances roads Blaxhall 
+            let ds = initialDistances rs Blaxhall 
             toList (updateDistance ds Blaxhall 15)  `shouldBe` 
                 [Blaxhall :-> 0
                 ,Clacton :-> 10000000
@@ -46,19 +49,19 @@ main = hspec $ do
                 ,Tiptree :-> 10000000]
     describe "shortestPathStep" $ do
         it "update the result for a step" $ do
-            let ds = initialDistances roads Blaxhall
-                (ds',p) = shortestPathStep roads (ds,[])
-            p  `shouldBe` [(Blaxhall,0)]        
+            let ds = initialDistances rs Blaxhall
+                (ds',p) = shortestPathStep rs (ds,Path [])
+            p  `shouldBe` Path [(Blaxhall,0)]        
         it "update the result for steps" $ do
-            let ds = initialDistances roads Blaxhall
-                (ds',p) = shortestPathStep roads (ds,[])
-                (ds'',p') = shortestPathStep roads (ds',p)
-            p'  `shouldBe`[(Blaxhall,0),(Dunwich,15)]
+            let ds = initialDistances rs Blaxhall
+                (ds',p) = shortestPathStep rs (ds,Path [])
+                (ds'',p') = shortestPathStep rs (ds',p)
+            p'  `shouldBe`Path [(Blaxhall,0),(Dunwich,15)]
         it "update the result for steps" $ do
-            let ds = initialDistances roads Blaxhall
-                step = shortestPathStep roads
-                (_,p) = step (step (step (step (step (step (step (ds,[])))))))
-            p `shouldBe` [(Blaxhall,0)
+            let ds = initialDistances rs Blaxhall
+                step = shortestPathStep rs
+                (_,p) = step (step (step (step (step (step (step (ds,Path [])))))))
+            p `shouldBe`Path  [(Blaxhall,0)
                          ,(Dunwich,15)
                          ,(Harwich,40)
                          ,(Feering,46)
