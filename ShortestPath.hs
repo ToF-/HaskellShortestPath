2754a2837b6c969c16b59e619ef4af2f8e5d876a
ToF- <cthibaut@octo.com>
2015-08-30 14:52:59 +0200

    trivial cases

--- ShortestPath.hs
+++ ShortestPath.hs
@@ -1,7 +1,15 @@
 module ShortestPath
 where
+import Data.PSQueue
 
 type WeighedGraph k w = [(k,[(k,w)])]
 
-shortestPath :: Ord k => Num w => WeighedGraph k w -> k -> k -> [(k,w)]
-shortestPath _ f t = [(f,0)]
+type Distances k w = PSQ k w
+
+shortestPath :: Ord (k) => Ord (w) => Num w => WeighedGraph k w -> k -> k -> [(k,w)]
+shortestPath g f t | f == t    = [(f,0)]
+                   | otherwise = case Prelude.lookup f g >>= Prelude.lookup t of
+                        Just n -> [(t,n)]
+
+initialDistances :: Ord k => Ord w => Num w => WeighedGraph k w -> k -> PSQ k w
+initialDistances g k = adjust (const 0) k (fromList (map (\(n,_) -> n :-> 10000000) g))
--- Specs.hs
+++ Specs.hs
@@ -1,9 +1,26 @@
 import Test.Hspec
 import Sample
 import ShortestPath
+import Data.PSQueue
 
 main = hspec $ do
     describe "shortest path" $ do
         it "from a node to itself is 0" $ do
             shortestPath roads Blaxhall Blaxhall `shouldBe` [(Blaxhall,0)] 
             shortestPath roads Clacton Clacton `shouldBe` [(Clacton,0)] 
+
+        it "from a node to a connected node is the weigh" $ do
+            shortestPath roads Blaxhall Dunwich `shouldBe` [(Dunwich,15)]
+
+    describe "initial distances" $ do
+        it "from a node yields all infinity except node" $ do
+            toList (initialDistances roads Blaxhall) `shouldBe`
+                [Blaxhall :-> 0
+                ,Clacton :-> 10000000
+                ,Dunwich :-> 10000000
+                ,Feering :-> 10000000
+                ,Harwich :-> 10000000
+                ,Maldon :-> 10000000
+                ,Tiptree :-> 10000000]
+    
+
