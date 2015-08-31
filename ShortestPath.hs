2c7240838bfd7a085c6e281beb72f5b81b858708
ToF- <cthibaut@octo.com>
2015-08-31 10:02:30 +0200

    starting over

--- ShortestPath.hs
+++ ShortestPath.hs
@@ -2,34 +2,39 @@ module ShortestPath
 where
 import Data.PSQueue (PSQ, adjust, fromList, Binding((:->)), minView)
 
-data Graph k w = Graph [(k,[(k,w)])]
-data Step k w = Step k w k deriving (Eq,Show)
-data Path k w = Path [Step k w] deriving (Eq,Show)
+type Graph k w = [(k,[(k,w)])]
+type Path k w = [(k,(w,k))]
 
-type Dists k w = PSQ k w
+shortestPath :: (Ord k, Ord w, Num w) => Graph k w -> k -> k -> Path k w
+shortestPath _ a z = [(z,(0,a))]
 
-shortestPath :: (Num w,Ord k, Ord w) => Graph k w -> k -> k -> Path k w
-shortestPath g f t =
-    let ds = initialDistances g f
-        loop g (ds,p@(Path (Step x _ _:ns))) | x == t = (ds,p)
-        loop g r = loop g $ shortestPathStep g r
-        (_,Path p) = loop g (ds, Path []) 
-    in Path $ reverse p 
-
-initialDistances :: (Ord k, Num w,Ord w) => Graph k w -> k -> Dists k w
-initialDistances (Graph g) k = adjust (const 0) k (fromList (map (\(n,_) -> n :-> 10000000) g))
-
-updateDistance :: (Ord k, Ord w) => Dists k w  -> k -> w -> Dists k w
-updateDistance ds k w = adjust (min w) k ds
-
-shortestPathStep :: (Ord w, Num w, Ord k) => Graph k w -> (Dists k w, Path k w) -> (Dists k w, Path k w) 
-shortestPathStep (Graph g) (ds,Path p) = case minView ds of
-    Nothing -> (ds,Path p)
-    Just (k :-> w, ds') -> let adj = lookup k g
-                               update ds (n,d) = updateDistance ds n (w+d)
-                               ds''= case adj of
-                                   Just ks -> foldl update ds' ks
-                                   Nothing -> ds'
-                           in (ds'', Path (Step k w k:p)) 
-
-                            
+-- data Step k w = Step k w k deriving (Eq,Show)
+-- data Path k w = Path [Step k w] deriving (Eq,Show)
+-- 
+-- type Dists k w = PSQ k w
+-- 
+-- shortestPath :: (Num w,Ord k, Ord w) => Graph k w -> k -> k -> Path k w
+-- shortestPath g f t =
+--     let ds = initialDistances g f
+--         loop g (ds,p@(Path (Step x _ _:ns))) | x == t = (ds,p)
+--         loop g r = loop g $ shortestPathStep g r
+--         (_,Path p) = loop g (ds, Path []) 
+--     in Path $ reverse p 
+-- 
+-- initialDistances :: (Ord k, Num w,Ord w) => Graph k w -> k -> Dists k w
+-- initialDistances (Graph g) k = adjust (const 0) k (fromList (map (\(n,_) -> n :-> 10000000) g))
+-- 
+-- updateDistance :: (Ord k, Ord w) => Dists k w  -> k -> w -> Dists k w
+-- updateDistance ds k w = adjust (min w) k ds
+-- 
+-- shortestPathStep :: (Ord w, Num w, Ord k) => Graph k w -> (Dists k w, Path k w) -> (Dists k w, Path k w) 
+-- shortestPathStep (Graph g) (ds,Path p) = case minView ds of
+--     Nothing -> (ds,Path p)
+--     Just (k :-> w, ds') -> let adj = lookup k g
+--                                update ds (n,d) = updateDistance ds n (w+d)
+--                                ds''= case adj of
+--                                    Just ks -> foldl update ds' ks
+--                                    Nothing -> ds'
+--                            in (ds'', Path (Step k w k:p)) 
+-- 
+--                             
--- Specs.hs
+++ Specs.hs
@@ -3,65 +3,11 @@ import Sample
 import ShortestPath
 import Data.PSQueue
 
-rs = Graph roads
+rs = roads
 
 
 main = hspec $ do
-    describe "shortest path" $ do
-        it "from a node to itself is 0" $ do
-            shortestPath rs Blaxhall Blaxhall `shouldBe` Path [(Blaxhall,0)] 
-            shortestPath rs Clacton Clacton `shouldBe` Path [(Clacton,0)] 
-
-        it "from a node to a connected node is the weigh" $ do
-            shortestPath rs Blaxhall Dunwich `shouldBe` Path [(Blaxhall,0),(Dunwich,15)]
-
-        it "for a whole graph is the shortest path" $ do
-            shortestPath rs Blaxhall Tiptree `shouldBe` Path [(Blaxhall,0),(Feering,46),(Tiptree,49)]
-
-    describe "initial distances" $ do
-        it "from a node yields all infinity except node" $ do
-            toList (initialDistances rs Blaxhall) `shouldBe`
-                [Blaxhall :-> 0
-                ,Clacton :-> 10000000
-                ,Dunwich :-> 10000000
-                ,Feering :-> 10000000
-                ,Harwich :-> 10000000
-                ,Maldon :-> 10000000
-                ,Tiptree :-> 10000000]
-
-    describe "update distance" $ do
-        it "update the distance table for a node if lower" $ do
-            let ds = initialDistances rs Blaxhall 
-            toList (updateDistance ds Dunwich 15)  `shouldBe` 
-                [Blaxhall :-> 0
-                ,Clacton :-> 10000000
-                ,Dunwich :-> 15
-                ,Feering :-> 10000000
-                ,Harwich :-> 10000000
-                ,Maldon  :-> 10000000
-                ,Tiptree :-> 10000000]
-        it "doesn't update the distance table for a node if higher" $ do
-            let ds = initialDistances rs Blaxhall 
-            toList (updateDistance ds Blaxhall 15)  `shouldBe` 
-                [Blaxhall :-> 0
-                ,Clacton :-> 10000000
-                ,Dunwich :-> 10000000
-                ,Feering :-> 10000000
-                ,Harwich :-> 10000000
-                ,Maldon  :-> 10000000
-                ,Tiptree :-> 10000000]
-    describe "shortestPathStep" $ do
-        it "update the result for a step" $ do
-            let ds = initialDistances rs Blaxhall
-                (ds',p) = shortestPathStep rs (ds,Path [])
-            p  `shouldBe` Path [(Blaxhall,0)]        
-        it "update the result for steps" $ do
-            let ds = initialDistances rs Blaxhall
-                (ds',p) = shortestPathStep rs (ds,Path [])
-                (ds'',p') = shortestPathStep rs (ds',p)
-            p'  `shouldBe`Path [(Dunwich,15),(Blaxhall,0)]
-        it "update the result for steps" $ do
-            let ds = initialDistances rs Blaxhall
-                step = shortestPathStep rs
-                (_,p) = step (step (step (step (step (step (step (ds,Path [])))))))
-            p `shouldBe` Path [(Maldon,57),(Clacton,57),(Tiptree,49),(Feering,46),(Harwich,40),(Dunwich,15),(Blaxhall,0)]
+    describe "shortestPath" $ do
+        it "from a node to itself is a one step path" $ do
+            shortestPath rs Blaxhall Blaxhall 
+                `shouldBe` [(Blaxhall,(0,Blaxhall))]
