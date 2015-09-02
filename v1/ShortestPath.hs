7999ef09fbf3a727e049aa50229827c068bef805
ToF- <cthibaut@octo.com>
2015-09-02 10:37:16 +0200

    path from a node to any node

--- v1/ShortestPath.hs
+++ v1/ShortestPath.hs
@@ -4,4 +4,5 @@ where
 pathTo :: (Eq n) => n -> [(n,(w,Maybe n))] -> [(n,w)]
 pathTo n dists = case lookup n dists of 
     Nothing -> error "node not in distance list"
-    Just (w,_) -> [(n,w)]
+    Just (w,Nothing) -> [(n,w)]
+    Just (w,Just m) -> pathTo m dists ++ [(n,w)]
--- v1/Specs.hs
+++ v1/Specs.hs
@@ -7,6 +7,14 @@ data City = A | B | C | D | E deriving (Eq,Ord,Show)
 main = hspec $ do
     describe "list of distances from a node" $ do
         describe "allows for computing the path" $ do
+            let dists = [(A,(0, Nothing)),(B,(3,Just A)),(C,(5,Just A)),(D,(7, Just B)),(E,(9,Just D))]
+            
             it "to the same node" $ do
-                let dists = [(A,(0, Nothing)),(B,(3,Just A)),(C,(5,Just A)),(D,(7, Just B)),(E,(9,Just D))]
                 pathTo A dists  `shouldBe` [(A,0)]
+
+            it "to the next node" $ do
+                pathTo B dists `shouldBe` [(A,0),(B,3)]
+
+        
+            it "to any node" $ do
+                pathTo E dists  `shouldBe` [(A,0),(B,3),(D,7),(E,9)]
