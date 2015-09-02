8c7329f43c106d8eebf2ead69c66e688d5c55c65
ToF- <cthibaut@octo.com>
2015-09-02 10:39:45 +0200

    presentation

--- v1/Specs.hs
+++ v1/Specs.hs
@@ -7,7 +7,11 @@ data City = A | B | C | D | E deriving (Eq,Ord,Show)
 main = hspec $ do
     describe "list of distances from a node" $ do
         describe "allows for computing the path" $ do
-            let dists = [(A,(0, Nothing)),(B,(3,Just A)),(C,(5,Just A)),(D,(7, Just B)),(E,(9,Just D))]
+            let dists = [(A,(0, Nothing))
+                        ,(B,(3,Just A))
+                        ,(C,(5,Just A))
+                        ,(D,(7, Just B))
+                        ,(E,(9,Just D))]
             
             it "to the same node" $ do
                 pathTo A dists  `shouldBe` [(A,0)]
