c9dd62673b83cd8f72e7d113c74183d044873849
ToF- <cthibaut@octo.com>
2015-09-05 20:07:25 +0200

    next distances

--- v2/Specs.hs
+++ v2/Specs.hs
@@ -7,11 +7,11 @@ data City = A | B | C | D
 
 
 main = hspec $ do
-    let g = fromList [(A,3,B),(B,2,C),(A,4,C),(C,1,D)]
+    let g = fromList [(A,3,B),(B,2,C),(A,7,C),(C,1,D)]
 
     describe "a graph" $ do
         it "allows for adjacent nodes to a node" $ do
-            adjacentNodes A g  `shouldBe` [(B,3),(C,4)]
+            adjacentNodes A g  `shouldBe` [(B,3),(C,7)]
             adjacentNodes B g  `shouldBe` [(A,3),(C,2)]
 
     describe "a distance queue" $ do
@@ -21,3 +21,18 @@ main = hspec $ do
                                   ,B :-> (10000, Nothing)
                                   ,C :-> (10000, Nothing)
                                   ,D :-> (10000, Nothing)]
+
+        it "is extracted with the closest node and updated" $ do
+            let ndg = nextDistances g
+                (q,l) = ndg (initialDistances A g,[])
+            Q.toList q `shouldBe` [B :-> (3,Just A)
+                                  ,C :-> (7,Just A)
+                                  ,D :-> (10000, Nothing)]
+
+            let (q,l) = ndg (ndg (initialDistances A g,[]))
+            Q.toList q `shouldBe` [C :-> (5,Just B)
+                                  ,D :-> (10000, Nothing)]
+
+            let (q,l) = ndg (ndg (ndg (initialDistances A g,[])))
+            Q.toList q `shouldBe` [D :-> (6, Just C)]
+            
